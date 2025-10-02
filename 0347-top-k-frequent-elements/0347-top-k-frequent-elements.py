a408b611dd1f1066eee626b2f2671e1b24aafdd7
Adrian Yu <adrian.yu001@gmail.com>
2025-10-02 18:53:53 +0700

    Time: 10 ms (25.55%), Space: 24.3 MB (13.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         occurrences = defaultdict(int)
+        length = len(nums) + 1
 
         for num in nums:
             occurrences[num] += 1
 
-        sorted_occurrences = sorted(occurrences, key=lambda num: occurrences[num], reverse=True)
+        frequencies = [[] for _ in range(length)]
+
+        for num, occur in occurrences.items():
+            frequencies[occur].append(num)
 
         top_elements = []
 
-        for i in range(k):
-            top_elements.append(sorted_occurrences[i])
+        for i in range(length):
+            nums = frequencies[length - i - 1]
+            
+            for num in nums:
+                top_elements.append(num)
 
-        return top_elements
+                if len(top_elements) == k:
+                    return top_elements
