a356ff07bbf9530fd4206a1d074c987ada933239
Adrian Yu <adrian.yu001@gmail.com>
2025-12-05 06:42:13 +0700

    Time: 1 ms (6.76%), Space: 17.8 MB (98.82%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,43 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def get_min_idx(left, right):
+            while left <= right:
+                mid = left + (right - left) // 2
+
+                if nums[mid] < nums[mid - 1]:
+                    return mid
+
+                if nums[mid] > nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+            return left
+
+        left = 0
+        right = len(nums) - 1
+        min_idx = get_min_idx(left, right)
+
+        print(f'Min Idx: {min_idx}')
+
+        # must be on the second array
+        if target < nums[0]:
+            left = min_idx
+        # must be on the first array
+        elif min_idx != left:
+            right = min_idx - 1
+
+        print(f'Left: {left}, Right: {right}')
+
+        # do another binary search based on the condition
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if target < nums[mid]:
+                right = mid - 1
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                return mid
+        
+        return -1
