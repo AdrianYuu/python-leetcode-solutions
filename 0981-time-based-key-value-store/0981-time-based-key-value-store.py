592403bca6a1bea523e3a32d5ccb47d5f2bb8b8e
Adrian Yu <adrian.yu001@gmail.com>
2025-12-16 12:10:23 +0700

    Time: 155 ms (18.93%), Space: 74.6 MB (39.52%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -0,0 +1,33 @@
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.time_map[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        current = self.time_map[key]
+
+        left = 0
+        right = len(current) - 1
+
+        result = ""
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            # current[mid][0] = timestamp
+            # current[mid][1] = value
+            if current[mid][0] <= timestamp:
+                result = current[mid][1]
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return result
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
